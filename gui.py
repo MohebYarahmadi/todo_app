9150fe91e5a4470da52497f11be032edbd3f6f73
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2025-03-29 20:03:08 +0330

    Implement edit logic

--- gui.py
+++ gui.py
@@ -3,25 +3,39 @@ import functions
 from gui_setting import *
 
 
-window = sg.Window('Todo App',
-                    layout=wrapper,
-                    font=('JetBrains Mono', 10)
-                    )
+window = sg.Window('Todo App', layout)
 
 while True:
     event, values = window.read()
-    print(event)
+    print(1, event)
+    print(2, values)
 
     match event:
-        case 'Exit':
+        case sg.WIN_CLOSED | 'Exit':
             break
-        case sg.WIN_CLOSED:
-            break
-        case 'Submit':
-            print('adding todo')
+        case 'Add':
             todos = functions._get_list()
-            todos.append(values["Add"] + "\n")
+            todos.append(values["todo"] + "\n")
             functions._post_list(todos)
+            window['item_select'].update(values=todos)
+            print('todo added')
+            
+        case 'item_select':
+            window['todo'].update(value=values['item_select'][0])
+            window['Edit'].update(disabled=False)
+            
+        case 'Edit':
+            current_item = values['item_select'][0]
+            new_item = values['todo']
+
+            todos = functions._get_list()
+            item_id = todos.index(current_item)
+            todos[item_id] = new_item + '\n'
+            functions._post_list(todos)
+            window['item_select'].update(values=todos)
+            window['Edit'].update(disabled=True)
+            print('todo updated')
+            
         case 'help':
             print('show help')
 
--- gui_setting.py
+++ gui_setting.py
@@ -1,20 +1,30 @@
 import FreeSimpleGUI as sg
+import functions
 
 # Global Settings
-sg.theme('Default1')
+sg.theme('Dark')
 
 
 # Components
-label = sg.Text('Type in a to-do')
-add_input_box = sg.InputText(key="Add")
-add_btn = sg.Submit()
-close_btn = sg.Exit(key='Exit')
-help_btn = sg.Button("Help", key='help')
+label1 = sg.Text('Type in a to-do')
+add_input_box = sg.InputText(key="todo", do_not_clear=False, size=47)
+add_btn = sg.Button("Add")
+list_box = sg.Listbox(values=functions._get_list(),
+                    key='item_select',
+                    bind_return_key=True,
+                    size=[45, 10])
+                    
+edit_btn = sg.Button("Edit", disabled=True)
+del_btn = sg.Button("Delete", disabled=True)
+
+close_btn = sg.Exit()
+help_btn = sg.Button("Help")
 
 
 # Create Wrapper
-wrapper= [
-    [label],
+layout= [
+    [label1],
     [add_input_box, add_btn],
+    [list_box, edit_btn],
     [help_btn, close_btn]
 ]
