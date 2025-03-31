54be10fe19e5d42b79a8dddd02df9d8f64c2f1a7
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2025-03-31 11:28:20 +0330

    Gui Final Touches

--- gui.py
+++ gui.py
@@ -1,19 +1,20 @@
 import functions
-
 from gui_setting import *
-
 
 window = sg.Window('Todo App', layout)
 
 while True:
     event, values = window.read()
-    print(1, event)
-    print(2, values)
+    # print(1, event)
+    # print(2, values)
 
     match event:
         case sg.WIN_CLOSED | 'Exit':
             break
         case 'Add':
+            if values['todo'] == '':
+                sg.popup('wrong input', title='Warning')
+                continue
             todos = functions._get_list()
             todos.append(values["todo"] + "\n")
             functions._post_list(todos)
@@ -23,6 +24,7 @@ while True:
         case 'item_select':
             window['todo'].update(value=values['item_select'][0])
             window['Edit'].update(disabled=False)
+            window['Done'].update(disabled=False)
             
         case 'Edit':
             current_item = values['item_select'][0]
@@ -35,8 +37,17 @@ while True:
             window['item_select'].update(values=todos)
             window['Edit'].update(disabled=True)
             print('todo updated')
+
+        case 'Done':
+            current_item = values['item_select'][0]
+
+            todos = functions._get_list()
+            todos.remove(current_item)
+            functions._post_list(todos)
+            window['item_select'].update(values=todos)
+            window['Done'].update(disabled=True)
             
-        case 'help':
-            print('show help')
+        case 'Help':
+            sg.popup('This is help dialog. you can use it later', title='Help')
 
 window.close()
--- gui_setting.py
+++ gui_setting.py
@@ -8,15 +8,18 @@ sg.theme('Dark')
 # Components
 label1 = sg.Text('Type in a to-do')
 add_input_box = sg.InputText(key="todo", do_not_clear=False, size=47)
-add_btn = sg.Button("Add")
+add_btn = sg.Button(key='Add', size=2, 
+                    image_source='files/add.png', 
+                    tooltip='Add new task')
 list_box = sg.Listbox(values=functions._get_list(),
                     key='item_select',
                     bind_return_key=True,
                     size=[45, 10])
                     
 edit_btn = sg.Button("Edit", disabled=True)
-del_btn = sg.Button("Delete", disabled=True)
-
+complete_btn = sg.Button(key="Done", disabled=True,
+                    image_source='files/complete.png',
+                    tooltip='Mark it Complete')
 close_btn = sg.Exit()
 help_btn = sg.Button("Help")
 
@@ -25,6 +28,6 @@ help_btn = sg.Button("Help")
 layout= [
     [label1],
     [add_input_box, add_btn],
-    [list_box, edit_btn],
+    [list_box, edit_btn, complete_btn],
     [help_btn, close_btn]
 ]
